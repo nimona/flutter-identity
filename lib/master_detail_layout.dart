4fa2ea0265b0fd6fc742992ce3d91ca9ca700574
George Antoniadis <george@noodles.gr>
2020-05-28 21:31:56 +0100

    v1.2

--- lib/master_detail_layout.dart
+++ lib/master_detail_layout.dart
@@ -2,11 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 import 'package:identity/data/repository.dart';
+import 'package:identity/view/identity/identity_created.dart';
 
 import 'package:identity_mobile/identity.dart';
 import 'package:local_auth_device_credentials/local_auth.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class MasterDetailLayout extends StatefulWidget {
@@ -34,10 +37,10 @@ class _MasterDetailLayoutState extends State<MasterDetailLayout> {
     super.dispose();
   }
 
-  void showDefaultSnackbar(BuildContext context) {
+  void showDefaultSnackbar(String s, BuildContext context) {
     Scaffold.of(context).showSnackBar(
       SnackBar(
-        content: Text('Public key copied'),
+        content: Text(s),
       ),
     );
   }
@@ -80,9 +83,13 @@ class _MasterDetailLayoutState extends State<MasterDetailLayout> {
     });
   }
 
+  bool _privateKeySeedVisible = false;
+  bool _privateKeyQRVisible = false;
+
   @override
   Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
+    final textTheme = Theme.of(context).textTheme;
+    final theme = Theme.of(context);
 
     removeIdentity(BuildContext context) {
       // set up the buttons
@@ -157,6 +164,7 @@ class _MasterDetailLayoutState extends State<MasterDetailLayout> {
           print("111!");
           print(snapshot.data);
           print(jsonEncode(snapshot.data));
+          List<String> words = snapshot.data.privateKeyMnemonic.split(" ");
 
           return Container(
             margin: EdgeInsets.all(10.0),
@@ -197,41 +205,240 @@ class _MasterDetailLayoutState extends State<MasterDetailLayout> {
                   alignment: Alignment.center,
                   // margin: EdgeInsets.all(10.0),
                   width: double.infinity,
-                  height: 80.0,
+                  // height: 80.0,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(10.0),
                     // border: Border.all(color: Colors.black26),
                   ),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.info_outline,
-                      size: 40.0,
-                    ),
-                    title: Text(
-                      "Public key",
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    subtitle: Text(
-                      snapshot.data.publicKey,
-                      textAlign: TextAlign.left,
-                      style: textTheme.bodyText2.copyWith(
-                        fontFamily: "Courier",
-                        color: Colors.grey,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    selected: false,
-                    onTap: () {
-                      Clipboard.setData(
-                        ClipboardData(
-                          text: snapshot.data.publicKey,
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(
+                          // Icons.info_outline,
+                          MaterialIcons.perm_identity,
+                          size: 40.0,
                         ),
-                      );
-                      showDefaultSnackbar(context);
-                    },
+                        title: Text(
+                          "Public key",
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        subtitle: Text(
+                          snapshot.data.publicKey,
+                          textAlign: TextAlign.left,
+                          style: textTheme.bodyText2.copyWith(
+                            fontFamily: "Courier",
+                            color: Colors.grey,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        selected: false,
+                        onTap: () {
+                          Clipboard.setData(
+                            ClipboardData(
+                              text: snapshot.data.publicKey,
+                            ),
+                          );
+                          showDefaultSnackbar("Public key copied", context);
+                        },
+                      ),
+
+                      // SEED
+
+                      SwitchListTile(
+                        secondary: Icon(
+                          Foundation.key,
+                          size: 40.0,
+                        ),
+                        title: Text(
+                          "Show private key seed",
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        subtitle: Text(
+                          "You should keep this private",
+                          textAlign: TextAlign.left,
+                          style: textTheme.bodyText2.copyWith(
+                            // fontFamily: "Courier",
+                            // color: Colors.red,
+                            color: Colors.grey,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        // selected: false,
+                        value: _privateKeySeedVisible,
+                        onChanged: (v) {
+                          // Clipboard.setData(
+                          //   ClipboardData(
+                          //     text: snapshot.data.publicKey,
+                          //   ),
+                          // );
+                          // showDefaultSnackbar(context);
+                          setState(() {
+                            _privateKeyQRVisible = false;
+                            _privateKeySeedVisible = v;
+                          });
+                        },
+                      ),
+                      () {
+                        if (!_privateKeySeedVisible) {
+                          return Container();
+                        }
+                        return Container(
+                          // margin: EdgeInsets.only(bottom: 15.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              // boxShadow: [
+                              //   BoxShadow(
+                              //     color: Colors.black26,
+                              //     blurRadius: 15.0,
+                              //   ),
+                              // ],
+                            ),
+                            padding: EdgeInsets.only(
+                              left: 5,
+                              right: 5,
+                              // bottom: 15,
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                WordsRow(index: 0, words: words),
+                                WordsRow(index: 3, words: words),
+                                WordsRow(index: 6, words: words),
+                                WordsRow(index: 9, words: words),
+                                WordsRow(index: 12, words: words),
+                                WordsRow(index: 15, words: words),
+                                WordsRow(index: 18, words: words),
+                                WordsRow(index: 21, words: words),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Container(
+                                  width: double.infinity,
+                                  child: RaisedButton(
+                                    child: Text("Copy to clipboard"),
+                                    onPressed: () {
+                                      Clipboard.setData(
+                                        ClipboardData(
+                                          text: words.join(" "),
+                                        ),
+                                      );
+                                      showDefaultSnackbar(
+                                        "Seed copied",
+                                        context,
+                                      );
+                                    },
+                                    color: theme.accentColor,
+                                    textColor: Colors.white,
+                                    // on
+                                  ),
+                                ),
+                                // Builder(
+                                //   builder: (BuildContext context) {
+                                //     return GestureDetector(
+                                //       onTap: () {
+                                //         Clipboard.setData(
+                                //           ClipboardData(
+                                //             text: () {
+                                //               var phrase = '';
+                                //               for (var i = 0;
+                                //                   i < words.length;
+                                //                   i++) {
+                                //                 var word = words[i];
+                                //                 if (i % 4 == 0) {
+                                //                   phrase =
+                                //                       phrase.trimRight() + "\n";
+                                //                 }
+                                //                 phrase +=
+                                //                     "${(i + 1).toString().padLeft(2, ' ')}";
+                                //                 phrase +=
+                                //                     ": ${word.padRight(10, ' ')}";
+                                //               }
+                                //               return phrase.trimRight();
+                                //             }(),
+                                //           ),
+                                //         );
+                                //         showDefaultSnackbar(
+                                //             "Seed copied", context);
+                                //       },
+                                //       child: Container(
+                                //         padding: EdgeInsets.only(
+                                //             top: 25, bottom: 10),
+                                //         child: Text(
+                                //           'Copy to clipboard',
+                                //           style: TextStyle(
+                                //             color: Color(0xFF6697FF),
+                                //             // fontSize: 14,
+                                //             fontWeight: FontWeight.bold,
+                                //           ),
+                                //         ),
+                                //       ),
+                                //     );
+                                //   },
+                                // ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }(),
+
+// QR CODE
+
+                      SwitchListTile(
+                        secondary: Icon(
+                          MaterialCommunityIcons.qrcode,
+                          size: 40.0,
+                        ),
+                        title: Text(
+                          "Show private key QR code",
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        subtitle: Text(
+                          "You should keep this private too",
+                          textAlign: TextAlign.left,
+                          style: textTheme.bodyText2.copyWith(
+                            // fontFamily: "Courier",
+                            // color: Colors.red,
+                            color: Colors.grey,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        // selected: false,
+                        value: _privateKeyQRVisible,
+                        onChanged: (v) {
+                          // Clipboard.setData(
+                          //   ClipboardData(
+                          //     text: snapshot.data.publicKey,
+                          //   ),
+                          // );
+                          // showDefaultSnackbar(context);
+                          setState(() {
+                            _privateKeySeedVisible = false;
+                            _privateKeyQRVisible = v;
+                          });
+                        },
+                      ),
+                      () {
+                        if (!_privateKeyQRVisible) {
+                          return Container();
+                        }
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 15.0),
+                          child: QrImage(
+                            data: words.join(" "),
+                            version: QrVersions.auto,
+                            size: 200.0,
+                          ),
+                        );
+                      }(),
+                    ],
                   ),
                 ),
                 SizedBox(
@@ -314,10 +521,52 @@ class _MasterDetailLayoutState extends State<MasterDetailLayout> {
                     },
                   ),
                 ),
+                SizedBox(
+                  height: 25,
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                // Text(
+                //   "Danger!",
+                //   textAlign: TextAlign.left,
+                //   style: headerStyle.copyWith(
+                //     color: Colors.red,
+                //   ),
+                // ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  padding: EdgeInsets.only(bottom: 15),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    onPressed: () {
+                      removeIdentity(context);
+                      // Repository.get().removeIdentity();
+                      // Navigator.pushNamedAndRemoveUntil(
+                      //   context,
+                      //   "/",
+                      //   (_) => false,
+                      // );
+                    },
+                    color: Colors.red,
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Remove identity',
+                          style: TextStyle(
+                            // fontSize: textTheme.bodyText2.fontSize,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           );
-
           // List<String> words = snapshot.data.identitySecretPhrase;
           //     return Container(
           //       padding: EdgeInsets.all(15),
